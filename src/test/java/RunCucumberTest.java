57f04bf570c72aa7fdc32c1c2c1b383084cebc47
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-04-19 18:16:57 -0300

    criando o runner do cucumber para que encontre os steps gerados

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,11 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepDefinitions" // Aqui é onde você especifica o pacote dos seus steps
+)
+public class RunCucumberTest {
+}
