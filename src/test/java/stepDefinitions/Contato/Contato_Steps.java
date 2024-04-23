4cd9be053ccf8159f1e9ae1a5e62f145de6f1132
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-04-22 21:45:27 -0300

    criando uma feature em pt-BR

---
+++ src/test/java/stepDefinitions/Contato/Contato_Steps.java
@@ -0,0 +1,32 @@
+package stepDefinitions.Contato;
+
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.it.Quando;
+
+public class Contato_Steps {
+    @Dado("que eu acesse a pagina de contato")
+    public void que_eu_acesse_a_pagina_de_contato() {
+        System.out.println("1");
+    }
+    @Quando("preencher meu nome")
+    public void preencher_meu_nome() {
+        System.out.println("2");
+    }
+    @Quando("sobrenome")
+    public void sobrenome() {
+        System.out.println("3");
+    }
+    @Quando("fazer um comentario")
+    public void fazer_um_comentario() {
+        System.out.println("4");
+    }
+    @Quando("enviar")
+    public void enviar() {
+        System.out.println("5");
+
+    }
+    @Quando("eu recebo a seguinte mensagem {string}")
+    public void eu_recebo_a_seguinte_mensagem(String string) {
+        System.out.println(string);
+    }
+}
---
+++ src/test/resources/features/Contato/Contato.feature
@@ -0,0 +1,12 @@
+ # language: pt
+ Funcionalidade: Contato com a universidade
+
+  Cenario: Validar o envio de contato
+
+    Dado que eu acesse a pagina de contato
+    Quando preencher meu nome
+    E sobrenome
+    E email
+    E fazer um comentario
+    E enviar
+    E eu recebo a seguinte mensagem "teste"
