d9ac03ed8ca3cc17218533fb3a5f57c253433d25
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-04-23 18:26:08 -0300

    esse comando gera o arquivo json, utilizado pelo cucu,ber reports no jenkins

--- src/test/java/runners/MainRunnerCucumber.java
+++ src/test/java/runners/MainRunnerCucumber.java
@@ -5,6 +5,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
                 tags = "@ingles", monochrome = true, dryRun = false, //dryRun é apenas pra rodar a stack e ver quais steps não foram implementados
-                plugin = {"pretty", "html:target/cucumber"})
+                plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class MainRunnerCucumber extends AbstractTestNGCucumberTests {
 }
