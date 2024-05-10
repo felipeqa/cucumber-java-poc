267358981b97f4c004e3536b089f60df57601111
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-10 13:39:41 -0300

    criamos o construtor para receber o valor das classes Teste1 e Teste2

--- src/test/java/stepDefinitions/POC_Dependencias/Teste3.java
+++ src/test/java/stepDefinitions/POC_Dependencias/Teste3.java
@@ -4,6 +4,15 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class Teste3 {
+
+    private final Teste1 teste1;
+    private final Teste2 teste2;
+
+    public Teste3(Teste1 teste1, Teste2 teste2) {
+        this.teste1 = teste1;
+        this.teste2 = teste2;
+    }
+
     @Então("entao valor teste3")
     public void entao_valor_teste3() {
 
