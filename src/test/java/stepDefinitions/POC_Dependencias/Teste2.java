ce6d66553406c7154618d9a55c3672a3b0bdedd9
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-10 12:33:15 -0300

    teste 2 implementado, mas precisamos carregar o valor1step1 pra fazer o calculo e posterior validação

--- src/test/java/stepDefinitions/POC_Dependencias/Teste2.java
+++ src/test/java/stepDefinitions/POC_Dependencias/Teste2.java
@@ -2,14 +2,19 @@ package stepDefinitions.POC_Dependencias;
 
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.testng.Assert;
 
 public class Teste2 {
+
+    private double valor2Step2;
+
     @Quando("quando valor teste2")
     public void quando_valor_teste2() {
-
+        valor2Step2 = 22.5;
     }
     @Então("entao valor teste2")
     public void entao_valor_teste2() {
-        
+        double total = valor2Step2 + valor1step1; //precisamos do valor1step1
+        Assert.assertEquals(total,30);
     }
 }
