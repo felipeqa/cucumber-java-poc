5866e29e3b3e516db672bfc2438e4dfc827a5c40
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-10 11:52:36 -0300

    teste 2 base, vai receber o valor do primeiro teste

---
+++ src/test/java/stepDefinitions/POC_Dependencias/Teste2.java
@@ -0,0 +1,15 @@
+package stepDefinitions.POC_Dependencias;
+
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class Teste2 {
+    @Quando("quando valor teste2")
+    public void quando_valor_teste2() {
+
+    }
+    @Então("entao valor teste2")
+    public void entao_valor_teste2() {
+        
+    }
+}
---
+++ src/test/resources/features/POC_Dependencias/Teste2.feature
@@ -0,0 +1,10 @@
+# language: pt
+
+@poc
+Funcionalidade: Compartilhar dados entre os steps Teste @
+
+  Cenario: Segundo teste Base
+
+    Dado dado valor teste1
+    Quando quando valor teste2
+    Então entao valor teste2
