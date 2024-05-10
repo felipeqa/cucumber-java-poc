aea9dea8af1804c11ce802f6e9f119cf0767f781
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-10 11:48:33 -0300

    teste base pra poc de compartilhar dados entre steps de classes diferentes

---
+++ src/test/java/stepDefinitions/POC_Dependencias/Teste1.java
@@ -0,0 +1,22 @@
+package stepDefinitions.POC_Dependencias;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class Teste1 {
+    @Dado("dado valor teste1")
+    public void dado_valor_teste1() {
+
+    }
+
+    @Quando("quando valor teste1")
+    public void quando_valor_teste1() {
+
+    }
+
+    @Então("entao valor teste1")
+    public void entao_valor_teste1() {
+
+    }
+}
---
+++ src/test/resources/features/POC_Dependencias/Teste1.feature
@@ -0,0 +1,10 @@
+# language: pt
+
+@poc
+Funcionalidade: Compartilhar dados entre os steps
+
+  Cenario: Primeiro teste Base
+
+    Dado dado valor teste1
+    Quando quando valor teste1
+    Então entao valor teste1
