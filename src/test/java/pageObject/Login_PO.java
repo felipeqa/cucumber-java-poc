838f38ac3b4a12f9abe91fc3ef6a05f7fa997122
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-11 22:28:34 -0300

    subindo a classe login page object

---
+++ src/test/java/pageObject/Login_PO.java
@@ -0,0 +1,30 @@
+package pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Login_PO extends Base_PO {
+
+    public Login_PO() {
+        super();
+    }
+
+    @FindBy(id = "text")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    public void visit(String url){
+        navigateTo_URL(url);
+    }
+
+    public void login (String user, String pass) {
+        sendKeys(username, user);
+        sendKeys(password, pass);
+        waitForWebElementAndClick(loginButton);
+    }
+}
